ea5eab2fd6336bf0541a47dafc0f3f18ae727bc1
aganci <aganci@gmail.com>
2017-05-01 11:16:11 +0200

    set status bar transparent

--- app/src/main/java/com/langhet/osterialanghet/MainActivity.java
+++ app/src/main/java/com/langhet/osterialanghet/MainActivity.java
@@ -1,8 +1,10 @@
 package com.langhet.osterialanghet;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -12,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        makeContentAppearBehindStatusBar();
+        setUpToolbar();
+    }
+
+    private void makeContentAppearBehindStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+    }
+
+    private void setUpToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Osteria Langhet");
